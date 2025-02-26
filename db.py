8abeecf1c66aea94b88711bb3cc51e5e11a40cd8
SculptTechProject <150788324+SculptTechProject@users.noreply.github.com>
2025-02-26 15:12:37 +0100

    working with static create

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from prisma import Prisma
 
 # Inicjalizacja klienta Prisma
-db = Prisma()
+db = Prisma(auto_register=True)
 
 async def connect_db():
     await db.connect()
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import asyncio
-from db import db, connect_db, disconnect_db
+#from db import db, connect_db, disconnect_db
+from prisma import Prisma, Json
 
 
-async def main():
-    await connect_db()  # Połączenie z bazą
+async def main() -> None:
+    #await connect_db()  # Połączenie z bazą
+    prisma = Prisma()
+    await prisma.connect()
 
-    # Tworzenie tabeli (automatycznie przy migracji)
-    await db.test.create(data={"name": "Example"})
+    processor = await prisma.processor.create(data = {
+        "producer": "AMD",
+        "name": "Ryzen 7 7800X",
+        "socket": "AM5",
+        "chipsets": Json({
+            'supported': ["B650", "X670", "X670E"]
+        }),
+        "architecture": "Zen 4",
+        "timing": "4.2 GHz Base / 5.4 GHz Boost",
+        "cores": 8,
+        "threads": 16,
+        "Unlocked_multiplier": True
+    })
 
-    # Pobranie wszystkich rekordów
-    records = await db.test.find_many()
-    print(records)
+    #await disconnect_db()
+    await prisma.disconnect()
 
-    await disconnect_db()  # Zamknięcie połączenia
-
-
-# Uruchomienie event loopa
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
